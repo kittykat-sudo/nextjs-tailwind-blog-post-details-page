0862262da0dd9bd2c5ab2df7806abce25ab63ed3
Induwara <induwara2k@gmail.com>
2024-01-15 20:02:41 +0530

    Revert "update the next js version to 14"
    This reverts commit dc02de990a3acc641ac30ce11195e752796a818f.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Roboto } from "next/font/google";
-import { Layout } from "@/components";
+import { FixedPlugin, Layout } from "@/components";
 
 const roboto = Roboto({
   subsets: ["latin"],
@@ -35,6 +35,7 @@ export default function RootLayout({
       <body className={roboto.className}>
         <Layout>
           {children}
+          <FixedPlugin />
         </Layout>
       </body>
     </html>
